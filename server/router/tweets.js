476ef4879dd2b214cd6ec11f51364b79d817ad9b
Minjeong Bak <doniminp@gmail.com>
2022-07-19 16:58:02 +0900

    Dreamcoding Node.js Dwitter clone project Rest API create

---
+++ server/router/tweets.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import 'express-async-errors';
+
+let tweets = [
+  {
+    id: '1',
+    text: '드림코딩 화이팅!',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+  },
+  {
+    id: '2',
+    text: '안뇽!',
+    createdAt: Date.now().toString(),
+    name: 'MinJeong',
+    username: 'ming',
+  },
+];
+
+const router = express.Router();
+
+// GET /tweets
+// GET /tweets?username=:username
+router.get('/', (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((tweet) => tweet.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+// GET /tweets/:id
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet ID(${id}) Not Found` });
+  }
+});
+/// POST /tweets
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+/// PUT /tweets/:id
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet ID(${id}) Not Found` });
+  }
+});
+/// DELETE /tweets/:id
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
+
+export default router;
