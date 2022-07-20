e586a752c78891b1321437f4422a23325643a343
Minjeong Bak <doniminp@gmail.com>
2022-07-20 16:52:12 +0900

    MVC 리팩토링

---
+++ server/data/tweet.js
@@ -0,0 +1,53 @@
+let tweets = [
+  {
+    id: '1',
+    text: '드림코딩 화이팅!',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+  },
+  {
+    id: '2',
+    text: '안뇽!',
+    createdAt: Date.now().toString(),
+    name: 'Ellie',
+    username: 'ellie',
+  },
+];
+
+export async function getAll() {
+  return tweets;
+}
+
+export async function getAllByUsername(username) {
+  return tweets.filter((tweet) => tweet.username === username);
+}
+
+export async function getAllById(id) {
+  return tweets.find((tweet) => tweet.id === id);
+}
+
+export async function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export async function update(id, text) {
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export async function remove(id) {
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+}
