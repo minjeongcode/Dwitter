aefb1e7e1a26e8d5b1e5d16987e91cf33de35c4e
Minjeong Bak <doniminp@gmail.com>
2022-07-30 15:47:16 +0900

    Configuration, Socket module 설치

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
+import { config } from './config.js';
 
 const app = express();
 
@@ -25,4 +26,5 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-app.listen(8080);
+console.log(config.host.port);
+app.listen(config.host.port);
